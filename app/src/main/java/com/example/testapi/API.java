29f0829ef268f20c12717798823f9a4357b99b35
Mark Pratt <mjpratt31@gmail.com>
2021-09-25 12:40:05 -0700

    Started with Volley

--- app/src/main/java/com/example/testapi/API.java
+++ app/src/main/java/com/example/testapi/API.java
@@ -1,9 +1,16 @@
 package com.example.testapi;
 
+import java.net.URI;
+
 public class API {
 
 
-    public boolean isWord(String word){
+    String APIKey = "de4ddcec3fmsh8fdbc2eee30df00p13e9a3jsn6ac2c2d3a074";
+    String requestURL = "https://wordsapiv1.p.rapidapi.com/words/%s";
+
+    public boolean isWord(String word) {
+
         return true;
+
     }
 }
